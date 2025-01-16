e4eaa8add84a45e11689f593276b3869be762c35
ssuukko <tlstpstpdl11@gmail.com>
2025-01-16 21:25:14 +0900

    feat: 프로젝트 생성

---
+++ src/test/java/jpa/fintech/FintechApplicationTests.java
@@ -0,0 +1,13 @@
+package jpa.fintech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
