d55d224255dbce3b083068595939eda3baa042bb
ssuukko <tlstpstpdl11@gmail.com>
2025-02-05 22:01:37 +0900

    feat: 수정

--- src/main/java/jpa/fintech/controller/LoginController.java
+++ src/main/java/jpa/fintech/controller/LoginController.java
@@ -1,33 +1,33 @@
 package jpa.fintech.controller;
 
-import jpa.fintech.dto.LoginDTO;
+import jpa.fintech.dto.request.LoginRequestDTO;
+import jpa.fintech.dto.response.LoginResponseDTO;
+import jpa.fintech.dto.response.ResponseAccountDTO;
+import jpa.fintech.service.AccountService;
 import jpa.fintech.service.LoginService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class LoginController {
 
     private final LoginService loginService;
-
-    @GetMapping("/login")
-    public String loginPage() {
-        return "login";
-    }
+    private final AccountService accountService;
 
     @PostMapping("/login")
-    public String login(@RequestBody LoginDTO loginDTO) {
-        boolean isAuthenticated = loginService.login(loginDTO);
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO loginRequestDTO) {
+        boolean isAuthenticated = loginService.login(loginRequestDTO.getUsername(), loginRequestDTO.getPassword());
         if (isAuthenticated) {
-            return "redirect:/home";
+            // 로그인 성공 후 계좌 목록 반환
+            List<ResponseAccountDTO> accountList = accountService.getAccountList(loginRequestDTO.getUsername());
+            return ResponseEntity.ok(new LoginResponseDTO("로그인 성공", accountList.toString()));
         } else {
-            return "redirect:/login?error=true";
+            return ResponseEntity.badRequest().body(new LoginResponseDTO("로그인 실패", null));
         }
-    }
-
-    @GetMapping("/home")
-    public String home() {
-        return "home";
     }
 }
